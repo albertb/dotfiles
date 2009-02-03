8370127e5be0abaf8687426879fd7a4b1b9686c2
Albert Bachand <albertb@gmail.com>
2009-02-02 22:59:42 -0800

    fix install script

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+rm -vf ${HOME}/.xmonad &&
+rm -vf ${HOME}/.vim &&
 for file in $(ls -A); do
   [ ${file} == "install.sh" ] ||
   [ ${file} == ".git" ] ||
-  ln -vsf ${PWD}/${file} ${HOME}/test/${file}
+  ln -vsF ${PWD}/${file} ${HOME}/${file}
 done
