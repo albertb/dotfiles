5237aba2d3bae73df756dc00b6c5e9d12a97ea13
Albert Bachand <albertb@gmail.com>
2009-12-10 17:40:12 -0800

    fix keys

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,40 +2,42 @@
 -- Author: albertb@gmail.com (Albert Bachand)
 
 import XMonad
+import System.Exit
 
-import qualified Data.Map as M
+import qualified XMonad.StackSet as W
+import qualified Data.Map        as M
 
 main :: IO ()
 main = xmonad $ defaultConfig
     { keys = myKeys
     , layoutHook = myLayoutHook
-    , manageHook = myManageHook
     , modMask = myModMask
     , terminal = myTerminal
     , workspaces = myWorkspaces
     }
 
--- TODO: h, t?
-myKeys :: XConfig Layout -> Map (KeyMask, KeySym) (X ())
-myKeys conf@(Xconfig {XMonad.modMask = modMask}) = M.fromList $
-    [ ((myModMask              , xK_a     ), refresh)
-    , ((myModMask .|.          , xK_c     ), kill)
-    , ((myModMask              , xK_d     ), sendMessage Shrink)
-    , ((myModMask              , xK_f     ), withFocused $ windows . W.sink)
-    , ((myModMask              , xK_n     ), sendMessage Expand)
-    , ((myModMask .|. shiftMask, xK_q     ), spawn "gnome-session-save --gui --kill")
-    , ((myModMask              , xK_q     ), restart "xmonad" True)
-    , ((myModMask .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
-    , ((myModMask              , xK_r     ), spawn "gmrun")
-    , ((myModMask              , xK_s     ), sendMessage NextLayout)
-    , ((myModMask .|. shiftMask, xK_s     ), setLayout $ XMonad.layoutHook conf)
-    , ((myModMask              , xK_space ), sendMessage NextLayout)
-    , ((myModMask .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
-    , ((myModMask              , xK_Tab   ), windows W.focusDown)
-    , ((myModMask .|. shiftMask, xK_Tab   ), windows W.focusUp)
-    , ((myModMask              , xK_v     ), sendMessage (IncMasterN (-1)))
-    , ((myModMask              , xK_w     ), sendMessage (IncMasterN 1))
-    , ((myModMask              , xK_x     ), spawn "xlock")
+myKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
+myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
+    [ ((modMask              , xK_a     ), refresh)
+    , ((modMask              , xK_c     ), kill)
+    , ((modMask              , xK_d     ), sendMessage Shrink)
+    , ((modMask              , xK_f     ), withFocused $ windows . W.sink)
+    , ((modMask .|. shiftMask, xK_h     ), windows W.focusDown)
+    , ((modMask              , xK_n     ), sendMessage Expand)
+    , ((modMask .|. shiftMask, xK_q     ), spawn "gnome-session-save --gui --kill")
+    , ((modMask              , xK_q     ), restart "xmonad" True)
+    , ((modMask .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
+    , ((modMask              , xK_r     ), spawn "gmrun")
+    , ((modMask              , xK_s     ), sendMessage NextLayout)
+    , ((modMask .|. shiftMask, xK_s     ), setLayout $ XMonad.layoutHook conf)
+    , ((modMask              , xK_space ), sendMessage NextLayout)
+    , ((modMask .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
+    , ((modMask              , xK_Tab   ), windows W.focusDown)
+    , ((modMask .|. shiftMask, xK_Tab   ), windows W.focusUp)
+    , ((modMask .|. shiftMask, xK_t     ), windows W.focusUp)
+    , ((modMask              , xK_v     ), sendMessage (IncMasterN (-1)))
+    , ((modMask              , xK_w     ), sendMessage (IncMasterN 1))
+    , ((modMask              , xK_x     ), spawn "xlock")
     ]
     ++
     [((m .|. myModMask, k), windows $ f i)
@@ -43,16 +45,13 @@ myKeys conf@(Xconfig {XMonad.modMask = modMask}) = M.fromList $
         , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
 
 myLayoutHook = tiled ||| Mirror tiled ||| Full
-    where tiled = Tall 1 (3 / 100)
+    where tiled = Tall 1 (3 / 100) (1 / 2)
 
-myManageHook :: [ManageHook]
-myManageHook = []
-
-myModMask :: KeySym
+myModMask :: KeyMask
 myModMask = mod4Mask
 
-myTreminal :: String
+myTerminal :: String
 myTerminal = "urxvt"
 
-myWorkspaces :: [WorkspaceID]
-myWorkspaces = ['α'..'κ']  -- FIXME
+myWorkspaces :: [WorkspaceId]
+myWorkspaces = [x:[] | x <- ['α'..'κ']]
