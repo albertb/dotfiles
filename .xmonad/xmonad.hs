91f40c80cae0383c756fc6deccfb02e0f829a981
Albert Bachand <albertb@chromium.org>
2009-12-17 14:56:54 -0800

    use xmobar

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,15 +2,20 @@
 -- Author: albertb@gmail.com (Albert Bachand)
 
 import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
 import System.Exit
+import System.IO
 
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
 
 main :: IO ()
-main = xmonad $ defaultConfig
+main = xmonad =<< xmobar defaultConfig
     { keys = myKeys
     , layoutHook = myLayoutHook
+    , manageHook = manageDocks <+> manageHook defaultConfig
     , modMask = myModMask
     , terminal = myTerminal
     , workspaces = myWorkspaces
@@ -19,13 +24,14 @@ main = xmonad $ defaultConfig
 myKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
 myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
     [ ((modMask              , xK_a     ), refresh)
-    , ((modMask              , xK_c     ), kill)
+    , ((modMask .|. shiftMask, xK_c     ), kill)
     , ((modMask              , xK_d     ), sendMessage Shrink)
     , ((modMask              , xK_f     ), withFocused $ windows . W.sink)
-    , ((modMask .|. shiftMask, xK_h     ), windows W.focusDown)
+    , ((modMask              , xK_h     ), windows W.focusDown)
     , ((modMask              , xK_n     ), sendMessage Expand)
-    , ((modMask .|. shiftMask, xK_q     ), spawn "gnome-session-save --gui --kill")
-    , ((modMask              , xK_q     ), restart "xmonad" True)
+    , ((modMask .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
+    , ((modMask              , xK_q     ), spawn "xmonad --recompile; xmonad --restart")
+    , ((modMask              , xK_Return), windows W.swapMaster)
     , ((modMask .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
     , ((modMask              , xK_r     ), spawn "gmrun")
     , ((modMask              , xK_s     ), sendMessage NextLayout)
@@ -34,7 +40,7 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
     , ((modMask .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
     , ((modMask              , xK_Tab   ), windows W.focusDown)
     , ((modMask .|. shiftMask, xK_Tab   ), windows W.focusUp)
-    , ((modMask .|. shiftMask, xK_t     ), windows W.focusUp)
+    , ((modMask              , xK_t     ), windows W.focusUp)
     , ((modMask              , xK_v     ), sendMessage (IncMasterN (-1)))
     , ((modMask              , xK_w     ), sendMessage (IncMasterN 1))
     , ((modMask              , xK_x     ), spawn "xlock")
@@ -44,14 +50,14 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
         | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
         , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
 
-myLayoutHook = tiled ||| Mirror tiled ||| Full
+myLayoutHook = avoidStruts $ tiled ||| Mirror tiled ||| Full
     where tiled = Tall 1 (3 / 100) (1 / 2)
 
 myModMask :: KeyMask
 myModMask = mod4Mask
 
 myTerminal :: String
-myTerminal = "urxvt"
+myTerminal = "gnome-terminal"  -- FIXME: setup antialias in urxvt
 
 myWorkspaces :: [WorkspaceId]
-myWorkspaces = [x:[] | x <- ['α'..'κ']]
+myWorkspaces = [show x | x <- [1..9]]
