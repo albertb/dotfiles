51c8841de2614b32408b658c0a570b110a85861d
Albert Bachand <albertb@gmail.com>
2010-04-22 13:52:29 -0700

    Fix home and end keys on OSX

--- install.sh
+++ install.sh
@@ -5,5 +5,11 @@ rm -rvf ${HOME}/.vim &&
 for file in $(ls -A); do
   [ ${file} == "install.sh" ] ||
   [ ${file} == ".git" ] ||
+  [ ${file} == "DefaultKeyBinding.dict" ] ||
   (rm ${HOME}/${file}; ln -vsF ${PWD}/${file} ${HOME}/${file})
 done
+
+if [[ $(uname) == "Darwin" ]]; then
+  mkdir ${HOME}/Library/KeyBindings &&
+  ln -vsF ${PWD}/DefaultKeyBinding.dict ${HOME}/Library/KeyBindings
+fi
