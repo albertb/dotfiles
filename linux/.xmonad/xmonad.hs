5eadbdb653f446f165b6aecee9fd67a8c416f553
Albert Bachand <albertb@gmail.com>
2020-02-28 11:35:00 -0800

    Submit current config

--- linux/.xmonad/xmonad.hs
+++ linux/.xmonad/xmonad.hs
@@ -1,8 +1,8 @@
 import XMonad
 import XMonad.Actions.CopyWindow
 import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ICCCMFocus
 import XMonad.Hooks.ManageDocks
+import XMonad.Layout.Spacing
 import XMonad.Layout.WindowNavigation
 import XMonad.Util.Run(spawnPipe)
 import System.Exit
@@ -12,19 +12,15 @@ import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
 
 main :: IO ()
-main = xmonad =<< xmobar defaultConfig
+main = xmonad =<< xmobar def
     { keys = myKeys
     , layoutHook = myLayoutHook
     , manageHook = myManageHook
     , normalBorderColor = "#000000"
     , borderWidth = 2
-    , modMask = myModMask
-    , terminal = myTerminal
-    , workspaces = myWorkspaces
-    , startupHook = do
-      takeTopFocus
-      --spawn "/usr/bin/xcompmgr"
-      spawn "xrandr --dpi 96x96"
+    , modMask = mod4Mask
+    , terminal = "urxvt"
+    , workspaces = [show x | x <- [1..9] ++ [0]]
     }
 
 myKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
@@ -42,28 +38,37 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
     , ((modMask              , xK_Return ), windows W.swapMaster)
     , ((modMask .|. shiftMask, xK_Return ), spawn $ XMonad.terminal conf)
     , ((modMask              , xK_r      ), spawn "gmrun")
-    , ((modMask              , xK_s      ), sendMessage NextLayout)
-    , ((modMask .|. shiftMask, xK_s      ), setLayout $ XMonad.layoutHook conf)
     , ((modMask              , xK_space  ), sendMessage NextLayout)
     , ((modMask .|. shiftMask, xK_space  ), setLayout $ XMonad.layoutHook conf)
-    , ((modMask              , xK_Tab    ), windows W.focusDown)
-    , ((modMask .|. shiftMask, xK_Tab    ), windows W.focusUp)
     , ((modMask              , xK_t      ), windows W.focusUp)
     , ((modMask .|. shiftMask, xK_t      ), sendMessage $ Swap U)
     , ((modMask              , xK_v      ), sendMessage (IncMasterN (-1)))
     , ((modMask              , xK_w      ), sendMessage (IncMasterN 1))
     , ((modMask              , xK_x      ), spawn "/usr/share/goobuntu-desktop-files/xsecurelock.sh")
+    , ((modMask              , xK_Right  ), sendMessage $ Go R)
+    , ((modMask              , xK_Left   ), sendMessage $ Go L)
+    , ((modMask              , xK_Up     ), sendMessage $ Go U)
+    , ((modMask              , xK_Down   ), sendMessage $ Go D)
+    , ((modMask .|. shiftMask, xK_Right  ), sendMessage $ Swap R)
+    , ((modMask .|. shiftMask, xK_Left   ), sendMessage $ Swap L)
+    , ((modMask .|. shiftMask, xK_Up     ), sendMessage $ Swap U)
+    , ((modMask .|. shiftMask, xK_Down   ), sendMessage $ Swap D)
     , ((0                    , xK_F9     ), spawn "~/bin/vol mute")
     , ((0                    , xK_F10    ), spawn "~/bin/vol minus")
     , ((0                    , xK_F11    ), spawn "~/bin/vol plus")
     ]
     ++
-    [((m .|. myModMask, k), windows $ f i)
+    [((m .|. modMask, k), windows $ f i)
         | (i, k) <- zip (XMonad.workspaces conf) ([xK_1 .. xK_9] ++ [xK_0])
         , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
 
-myLayoutHook = windowNavigation (avoidStruts $ tiled ||| Full)
-    where tiled = Tall 1 (3 / 100) (1 / 2)
+myLayoutHook =
+    avoidStruts $
+    windowNavigation $
+    spacingRaw True (Border 0 2 2 2) True (Border 2 2 2 2) True
+        (tiled ||| Mirror tiled ||| Full)
+    where
+      tiled = Tall 1 (3/100) (3/5)
 
 myManageHook :: ManageHook
 myManageHook = composeAll
@@ -72,14 +77,6 @@ myManageHook = composeAll
       ) --> doFloat
     , manageDocks
     ]
-    <+> manageHook defaultConfig
+    <+> manageHook def
     where role = stringProperty "WM_WINDOW_ROLE"
 
-myModMask :: KeyMask
-myModMask = mod4Mask
-
-myTerminal :: String
-myTerminal = "urxvt"
-
-myWorkspaces :: [WorkspaceId]
-myWorkspaces = [show x | x <- [1..9] ++ [0]]
